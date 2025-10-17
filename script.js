74346e1f2fed4be267a3d5328aeef5c379df8531
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-10-17 15:52:55 +0200

    Add divide function

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
     console.log("Calculatrice initialisée !");
   });
+
+  export function divide(a, b) {
+    return a / b;
+  }
---
+++ tests/calc.test.js
@@ -0,0 +1,5 @@
+import { divide } from "../script.js";
+
+test("division simple", () => {
+  expect(divide(4, 2)).toBe(2);
+});
